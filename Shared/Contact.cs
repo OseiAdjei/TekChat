ca712e61245ae4e17edef0ee70912d5a561210c8
kwabs digital <kwabs digital@DESKTOP-F8MDC1K>
2023-11-21 10:10:42 +0000

    Add project files.

---
+++ Shared/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekChat.Shared
+{
+    public  class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+        public Contact(int contactId, string firstName, string lastName)
+        {
+            this.ContactId = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
